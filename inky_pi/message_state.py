887f0cb5fa2d13a10e549da5499ef5f3201233f9
ian-antking <ian.antking@gmail.com>
2020-05-24 19:13:33 +0100

    move screen logic to screen controller

--- inky_pi/message_state.py
+++ inky_pi/message_state.py
@@ -1,60 +1,27 @@
 #!/usr/bin/env python
-from state import State
-
 import paho.mqtt.client as mqtt
 import json
-from inky import InkyPHAT
-from PIL import Image, ImageFont, ImageDraw
-from font_fredoka_one import FredokaOne
+from state import State
+from screen_controller import ScreenController
 
 class MessageState(State):
   def __init__(self):
     super().__init__('message')
-    self.inky_display = InkyPHAT('red')
-    self.inky_display.set_border(self.inky_display.BLACK)
+    self.screen_controller = ScreenController()
     self.client = mqtt.Client('inky')
     self.client.on_message = self.on_message
     self.client.connect('192.168.1.128')
 
   def enterState(self):
-    img = Image.new("P", (self.inky_display.WIDTH, self.inky_display.HEIGHT), self.inky_display.BLACK)
-    draw = ImageDraw.Draw(img)
-    self.inky_display.set_image(img)
-    self.inky_display.show()
+    self.screen_controller.display_message('Message Mode')
 
     self.client.loop_start()
     self.client.subscribe('test/message')
-  
-  def display_message(self, message):
-    message_text = message['text']
-    message_author = "--" + message['author']
-
-    img = Image.new("P", (self.inky_display.WIDTH, self.inky_display.HEIGHT), self.inky_display.BLACK)
-
-    draw = ImageDraw.Draw(img)
-
-    message_font = ImageFont.truetype(FredokaOne, 18)
-    author_font = ImageFont.truetype(FredokaOne, 16)
-
-    tw, th = message_font.getsize(message_text)
-    aw, ah = author_font.getsize(message_author)
-
-    text_x = (self.inky_display.WIDTH / 2) - (tw / 2)
-    text_y = (self.inky_display.HEIGHT / 3) - (th / 2)
-
-    author_x = (self.inky_display.WIDTH / 3) * 2 - (aw /  2)
-    author_y = (self.inky_display.HEIGHT / 3) * 2 - (ah / 2)
-
-    draw.text((text_x, text_y), message_text, self.inky_display.WHITE, message_font)
-    draw.text((author_x, author_y), message_author, self.inky_display.RED, author_font)
-    self.inky_display.set_image(img)
-
-    self.inky_display.show()
 
   def on_message(self, client, userdata, message):
       payload = str(message.payload.decode("utf-8", "ignore"))
       payload_dictionary = json.loads(payload)
-      self.display_message(payload_dictionary)
+      self.screen_controller.display_quote(payload_dictionary)
 
   def exitState(self):
     self.client.loop_stop()
---
+++ inky_pi/screen_controller.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from inky import InkyPHAT
+from PIL import Image, ImageFont, ImageDraw
+from font_fredoka_one import FredokaOne
+
+class ScreenController():
+  def __init__(self):
+        self.inky_display = InkyPHAT('red')
+        self.inky_display.set_border(self.inky_display.BLACK)
+
+  def display_quote(self, message):
+    message_text = message['text']
+    message_author = "--" + message['author']
+
+    img = Image.new("P", (self.inky_display.WIDTH, self.inky_display.HEIGHT), self.inky_display.BLACK)
+
+    draw = ImageDraw.Draw(img)
+
+    message_font = ImageFont.truetype(FredokaOne, 18)
+    author_font = ImageFont.truetype(FredokaOne, 16)
+
+    tw, th = message_font.getsize(message_text)
+    aw, ah = author_font.getsize(message_author)
+
+    text_x = (self.inky_display.WIDTH / 2) - (tw / 2)
+    text_y = (self.inky_display.HEIGHT / 3) - (th / 2)
+
+    author_x = (self.inky_display.WIDTH / 3) * 2 - (aw /  2)
+    author_y = (self.inky_display.HEIGHT / 3) * 2 - (ah / 2)
+
+    draw.text((text_x, text_y), message_text, self.inky_display.WHITE, message_font)
+    draw.text((author_x, author_y), message_author, self.inky_display.RED, author_font)
+    self.inky_display.set_image(img)
+
+    self.inky_display.show()
+
+  def display_message(self, message):
+    img = Image.new("P", (self.inky_display.WIDTH, self.inky_display.HEIGHT), self.inky_display.BLACK)
+
+    draw = ImageDraw.Draw(img)
+
+    message_font = ImageFont.truetype(FredokaOne, 18)
+    author_font = ImageFont.truetype(FredokaOne, 16)
+
+    tw, th = message_font.getsize(message_text)
+
+    text_x = (self.inky_display.WIDTH / 2) - (tw / 2)
+    text_y = (self.inky_display.HEIGHT / 2) - (th / 2)
+
+    draw.text((text_x, text_y), message_text, self.inky_display.WHITE, message_font)
+    self.inky_display.set_image(img)
+
+    self.inky_display.show()
