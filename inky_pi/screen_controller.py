61e6a79413885f60152c44df41dbe35c10c3fa2e
ian-antking <ian.antking@gmail.com>
2020-05-24 23:30:45 +0100

    reduce message font size

--- inky_pi/screen_controller.py
+++ inky_pi/screen_controller.py
@@ -33,8 +33,8 @@ class ScreenController():
 
     draw = ImageDraw.Draw(img)
 
-    message_font = ImageFont.truetype(FredokaOne, 18)
-    author_font = ImageFont.truetype(FredokaOne, 16)
+    message_font = ImageFont.truetype(FredokaOne, 16)
+    author_font = ImageFont.truetype(FredokaOne, 14)
 
     message_text = self.reflow_message(message['text'], int(self.inky_display.WIDTH * 0.9), message_font)
     message_author = "--" + message['author']
