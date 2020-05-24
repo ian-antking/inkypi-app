b6f36537695c23fa8d709b620375a1200790abe6
ian-antking <ian.antking@gmail.com>
2020-05-24 19:27:00 +0100

    connect modes to buttons

--- inky_pi/app.py
+++ inky_pi/app.py
@@ -31,21 +31,22 @@ if __name__ == '__main__':
   @buttonshim.on_press(buttonshim.BUTTON_A)
   def button_a(button, pressed):
     buttonshim.set_pixel(0x94, 0x00, 0xd3)
-
+    app.setMode('mode a')
 
   @buttonshim.on_press(buttonshim.BUTTON_B)
   def button_b(button, pressed):
     buttonshim.set_pixel(0x00, 0x00, 0xff)
-
+    app.setMode('mode b')
 
   @buttonshim.on_press(buttonshim.BUTTON_C)
   def button_c(button, pressed):
     buttonshim.set_pixel(0x00, 0xff, 0x00)
-
+    app.setMode('mode c')
 
   @buttonshim.on_press(buttonshim.BUTTON_D)
   def button_d(button, pressed):
     buttonshim.set_pixel(0xff, 0xff, 0x00)
+    app.setMode('mode d')
 
   @buttonshim.on_press(buttonshim.BUTTON_E)
   def button_e(button, pressed):
