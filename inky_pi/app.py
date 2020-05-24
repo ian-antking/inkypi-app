b576aa7814a6f398b3e4dfedf5bd108346e06074
ian-antking <ian.antking@gmail.com>
2020-05-24 15:37:51 +0100

    add button listeners

---
+++ inky_pi/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+
+class App():
+    def __init__(self):
+      print('app starting')
+
+
+if __name__ == '__main__':
+  import buttonshim
+  import signal
+
+  @buttonshim.on_press(buttonshim.BUTTON_A)
+  def button_a(button, pressed):
+      buttonshim.set_pixel(0x94, 0x00, 0xd3)
+
+
+  @buttonshim.on_press(buttonshim.BUTTON_B)
+  def button_b(button, pressed):
+      buttonshim.set_pixel(0x00, 0x00, 0xff)
+
+
+  @buttonshim.on_press(buttonshim.BUTTON_C)
+  def button_c(button, pressed):
+      buttonshim.set_pixel(0x00, 0xff, 0x00)
+
+
+  @buttonshim.on_press(buttonshim.BUTTON_D)
+  def button_d(button, pressed):
+      buttonshim.set_pixel(0xff, 0xff, 0x00)
+
+
+  @buttonshim.on_press(buttonshim.BUTTON_E)
+  def button_e(button, pressed):
+      buttonshim.set_pixel(0xff, 0x00, 0x00)
+
+
+  signal.pause()
