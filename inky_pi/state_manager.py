fe5c5585f139ee2fd75598ef13e4cc4fbdf52bbd
ian-antking <ian.antking@gmail.com>
2020-05-24 18:16:05 +0100

    call enterState in __init__

--- inky_pi/state_manager.py
+++ inky_pi/state_manager.py
@@ -4,6 +4,7 @@ class StateManager:
   def __init__(self, states):
     self.states = states
     self.state = states[0]
+    self.state['enterState']()
 
   def findState(self, state_name):
     return next((state for state in self.states if state.name == state_name), None)
