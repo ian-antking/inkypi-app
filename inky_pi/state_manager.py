79a57ae91bda021bb2f0e17d4dc72e73484f0c57
ian-antking <ian.antking@gmail.com>
2020-05-24 18:17:40 +0100

    call methods with dot notation

--- inky_pi/state_manager.py
+++ inky_pi/state_manager.py
@@ -4,15 +4,15 @@ class StateManager:
   def __init__(self, states):
     self.states = states
     self.state = states[0]
-    self.state['enterState']()
+    self.state.enterState()
 
   def findState(self, state_name):
     return next((state for state in self.states if state.name == state_name), None)
 
   def setState(self, state):
-    self.state['exitState']()
+    self.state.exitState()
     self.state = self.findState(state)
-    self.state['enterState']()
+    self.state.enterState()
   
   def getState(self):
     return self.state['name']
