79602cc42e8acc6e3dc49c9ab8ad552168403cae
ian-antking <ian.antking@gmail.com>
2020-05-24 22:55:31 +0100

    instantly display new messages

--- inky_pi/message_state.py
+++ inky_pi/message_state.py
@@ -9,6 +9,7 @@ class MessageState(State):
   def __init__(self):
     super().__init__('message')
     self.messages = []
+    self.new_messages = []
     self.current_message_index = 0
     self.update_timer = 0
     self.screen_controller = ScreenController()
@@ -28,10 +29,14 @@ class MessageState(State):
     self.messages.append(payload_dictionary)
 
   def update(self):
-    if len(self.messages) > 1 and self.update_timer == 60:
+    if len(self.new_messages):
+      self.screen_controller.display_quote(self.new_messages[0])
+      self.messages.append(self.new_messages.pop(0))
+    elif len(self.messages) > 1 and self.update_timer == 60:
       self.screen_controller.display_quote(self.messages[self.current_message_index % len(self.messages)])
       self.current_message_index += 1
       self.update_timer = 0
 
     sleep(1)
     self.update_timer += 1
+    print(self.update_timer, self.messages)
