dd220ea540f015aa6a4e2d729d1854590768f5c3
ian-antking <ian.antking@gmail.com>
2020-05-24 17:30:21 +0100

    StateManager expects a list of states

--- inky_pi/state_manager.py
+++ inky_pi/state_manager.py
@@ -3,38 +3,15 @@
 class StateManager:
   def __init__(self, states):
     self.states = states
-    self.state = list(states.values())[0]
+    self.state = states[0]
+
+  def findState(self, state_name):
+    return next((state for state in self.states if state.name == state_name), None)
 
   def setState(self, state):
     self.state['exit']()
-    self.state = self.states[state]
+    self.state = self.findState(state)
     self.state['enter']()
   
   def getState(self):
     return self.state['name']
-
-if __name__ == '__main__':
-  states = {
-    'namebadge': {
-      'name': 'namebadge',
-      'enter': lambda : print('setting mode to namebadge'),
-      'execute': lambda : print('executing namebadge'),
-      'exit': lambda : print('exiting nambadge')
-    },
-    'message': {
-      'name': 'message',
-      'enter': lambda : print('setting mode to message'),
-      'execute': lambda : print('executing message'),
-      'exit': lambda : print('exiting message')
-    }
-  }
-
-  state_manager = StateManager(states)
-
-  state_manager.state['execute']()
-
-  print(state_manager.getState())
-
-  state_manager.setState('message')
-
-  state_manager.state['execute']()
