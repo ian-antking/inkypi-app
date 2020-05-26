76faab73656d5e3199944d3fa4ccb415d95e2f9b
ian-antking <ian.antking@gmail.com>
2020-05-26 22:25:23 +0100

    add screen constructor

---
+++ inky_pi/screen.py
@@ -0,0 +1,12 @@
+from PIL import Image, ImageFont, ImageDraw
+from font_source_serif_pro import SourceSerifProSemibold
+from font_source_sans_pro import SourceSansProSemibold
+from theme import Theme
+
+class Screen():
+  def __init__(self, colour_scheme, inky_display):
+    self.theme = Theme(colour_scheme, inky_display)
+    self.width = inky_display.WIDTH
+    self.height = inky_display.HEIGHT
+    self.background = Image.new('P', (self.width, self.height), self.theme.background)
+    self.draw = ImageDraw.Draw(self.background)
