cf681466381da7dc302604fd1de190d9b48f5e3f
ian-antking <ian.antking@gmail.com>
2020-05-26 22:38:32 +0100

    remove unused imports

--- inky_pi/screen.py
+++ inky_pi/screen.py
@@ -1,6 +1,4 @@
 from PIL import Image, ImageFont, ImageDraw
-from font_source_serif_pro import SourceSerifProSemibold
-from font_source_sans_pro import SourceSansProSemibold
 from theme import Theme
 
 class Screen():
