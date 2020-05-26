bc4a25b1bb8dfecdb752aeafa925626147e49fba
ian-antking <ian.antking@gmail.com>
2020-05-26 22:39:35 +0100

    use Screen constructor in all methods

--- inky_pi/screen_controller.py
+++ inky_pi/screen_controller.py
@@ -3,7 +3,6 @@ from inky import InkyPHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_source_serif_pro import SourceSerifProSemibold
 from font_source_sans_pro import SourceSansProSemibold
-from theme import Theme
 from screen import Screen
 
 class ScreenController():
@@ -35,10 +34,7 @@ class ScreenController():
     return reflowed
 
   def display_message(self, message, theme = 'light'):
-    theme = Theme(message['theme'] or theme, self.inky_display)
-    img = Image.new("P", (self.screen_width, self.screen_height), theme.background)
-
-    draw = ImageDraw.Draw(img)
+    screen = Screen(message['theme'] or theme, self.inky_display)
 
     message_font = ImageFont.truetype(SourceSerifProSemibold, 16)
     author_font = ImageFont.truetype(SourceSansProSemibold, 18)
@@ -57,9 +53,9 @@ class ScreenController():
     author_x = self.screen_width - (aw + int(self.screen_width * 0.1))
     author_y = self.screen_height - (ah + int(self.screen_height * 0.1))
 
-    draw.multiline_text((text_x, text_y), message_text, theme.text, message_font)
-    draw.text((author_x, author_y), message_author, theme.highlight, author_font)
-    self.inky_display.set_image(img)
+    screen.draw.multiline_text((text_x, text_y), message_text, screen.theme.text, message_font)
+    screen.draw.text((author_x, author_y), message_author, screen.theme.highlight, author_font)
+    self.inky_display.set_image(screen.image)
 
     self.inky_display.show()
 
@@ -88,10 +84,7 @@ class ScreenController():
     self.inky_display.show()
 
   def display_prompt(self, message, instruction, theme = 'light'):
-    theme = Theme(theme, self.inky_display)
-    img = Image.new("P", (self.screen_width, self.screen_height), theme.background)
-
-    draw = ImageDraw.Draw(img)
+    screen = Screen(theme, self.inky_display)
 
     message_font = ImageFont.truetype(SourceSansProSemibold, 18)
     instruction_font = ImageFont.truetype(SourceSansProSemibold, 16)
@@ -108,17 +101,17 @@ class ScreenController():
     instruction_x = (self.screen_width  / 2) - (iw / 2)
     instruction_y = ((self.screen_height / 3) * 2)
 
-    draw.rectangle((0, 0, self.screen_width, (self.screen_height - th) / 2), fill=theme.highlight)
+    screen.draw.rectangle((0, 0, self.screen_width, (self.screen_height - th) / 2), fill=screen.theme.highlight)
 
     hatch_spacing = 24
 
     for x in range(0, 2 * self.screen_width, hatch_spacing):
-      draw.line((x, 0, x - self.screen_width, self.screen_height), fill=theme.background, width=3)
+      screen.draw.line((x, 0, x - self.screen_width, self.screen_height), fill=screen.theme.background, width=3)
 
-    draw.rectangle((0, text_y + th + 5, self.screen_width, self.screen_height), fill=theme.highlight)
+    screen.draw.rectangle((0, text_y + th + 5, self.screen_width, self.screen_height), fill=screen.theme.highlight)
 
-    draw.multiline_text((text_x, text_y), message_text, theme.text, message_font, align='center')
-    draw.multiline_text((instruction_x, instruction_y), instruction_text, theme.background, instruction_font, align='center')
-    self.inky_display.set_image(img)
+    screen.draw.multiline_text((text_x, text_y), message_text, screen.theme.text, message_font, align='center')
+    screen.draw.multiline_text((instruction_x, instruction_y), instruction_text, screen.theme.background, instruction_font, align='center')
+    self.inky_display.set_image(screen.image)
     self.inky_display.show()
 
