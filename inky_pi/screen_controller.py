0382bc6cdf9211aab4bba6f8224140afecba0afd
ian-antking <ian.antking@gmail.com>
2020-05-26 23:51:59 +0100

    remove unused method

--- inky_pi/screen_controller.py
+++ inky_pi/screen_controller.py
@@ -14,28 +14,6 @@ class ScreenController():
         self.screen_width = self.inky_display.WIDTH
         self.screen_height = self.inky_display.HEIGHT
 
-  def reflow_message(self, message, width, font, quote=True):
-    words = message.split(" ")
-    reflowed = '"' if quote else ''
-    line_length = 0
-
-    end_of_string = '"' if quote else ''
-
-    for i in range(len(words)):
-        word = words[i] + " "
-        word_length = font.getsize(word)[0]
-        line_length += word_length
-
-        if line_length < width:
-            reflowed += word
-        else:
-            line_length = word_length
-            reflowed = reflowed[:-1] + "\n  " + word
-
-    reflowed = reflowed.rstrip() + end_of_string
-
-    return reflowed
-
   def display_message(self, message, theme = 'light'):
     screen = MessageScreen(message['theme'] or theme, self.inky_display, message)
     self.inky_display.set_image(screen.image)
