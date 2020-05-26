dfdd1c54d70856cbaffcf78c07d1079a7bb14b65
ian-antking <ian.antking@gmail.com>
2020-05-26 12:05:08 +0100

    pass actual message text to screen_controller

--- inky_pi/message_state.py
+++ inky_pi/message_state.py
@@ -38,7 +38,7 @@ class MessageState(State):
       message = self.new_messages.pop(0)
       print(message)
       self.messages.append(message)
-      self.screen_controller.display_message('This is the message')
+      self.screen_controller.display_message(message['text'])
     
     print(self.new_messages)
     print(self.messages)
