a7b35dcd0c31bf158dc131105ddf3c0df8283e3c
ian-antking <ian.antking@gmail.com>
2020-06-08 13:28:09 +0100

    correct class name

--- inky_pi/menu_state.py
+++ inky_pi/menu_state.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import json
 from state import State
 
-class MessageState(State):
+class MenuState(State):
   def __init__(self, state_name):
     super().__init__(state_name)
 
