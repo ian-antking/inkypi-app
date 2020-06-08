f1133d605c8ed5ac73eed58ce6406333990e4264
ian-antking <ian.antking@gmail.com>
2020-06-08 16:18:30 +0100

    load dotenve in states module

--- inky_pi/states.py
+++ inky_pi/states.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from message_state import MessageState
 from menu_state import MenuState
 
+load_dotenv()
+
 mqtt_broker = os.getenv('MQTT_BROKER')
 message_topic = os.getenv('MESSAGE_TOPIC')
 
